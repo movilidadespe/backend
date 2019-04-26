0d50f3d0fcea82cbaf9656c8a5462a2889e8ffea
joseobando0001 <jose.obando_0001@hotmail.com>
2019-04-26 11:19:31 -0500

    Actualizacion del SELECT

--- crud/src/main/java/com/espe/crud/controllers/añosController.java
+++ crud/src/main/java/com/espe/crud/controllers/añosController.java
@@ -83,9 +83,90 @@ public class añosController {
 	
 	@GetMapping("/requisito/{id}")
 	public List<ReqplanmVo> requisito1(@PathVariable Long id) throws SQLException{
-		String q = "INSERT WHEN (SELECT TRUNC((( SYSDATE - PEBEMPL_FIRST_HIRE_DATE )/365),0)  AS TOTAL FROM PEBEMPL WHERE PEBEMPL_PIDM = " + id +" AND PEBEMPL_BCAT_CODE = 'DO') >3 THEN\r\n" + 
-				" into utic.uzmtverireq(uzmtverireq_id, uzmtreqplanm_id, PEAEMPL_PIDM)\r\n" + 
-				" select 2,3, "+ id +" from dual";
+		String q = "insert\r\n" + 
+				"WHEN (SELECT TRUNC((( SYSDATE - PEBEMPL_FIRST_HIRE_DATE )/365),0) AS TOTAL FROM PEBEMPL WHERE PEBEMPL_PIDM = "+ id +" AND PEBEMPL_BCAT_CODE = 'DO') >3 THEN\r\n" + 
+				"into utic.uzmtverireq(uzmtverireq_id, uzmtreqplanm_id, PEAEMPL_PIDM)VALUES (2,3," + id + ")\r\n" + 
+				"WHEN ((SELECT DISTINCT\r\n" + 
+				"(select max(ptrtenr_desc)  from PTRTENR where ptrtenr_code= PERAPPT_TENURE_CODE ) as CATEGORIA_ESCALAFON\r\n" + 
+				"FROM PEBEMPL r, PERAPPT\r\n" + 
+				"WHERE\r\n" + 
+				"r.pebempl_PIDM = PERAPPT.PERAPPT_PIDM\r\n" + 
+				"AND r.pebempl_empl_status = 'A'\r\n" + 
+				"--AND( r.pebempl_bcat_code = 'DO' )\r\n" + 
+				"AND r.pebempl_PIDM = "+ id +"\r\n" + 
+				"AND (r.pebempl_bcat_code = 'DO' or r.pebempl_bcat_code = 'SP' ) AND\r\n" + 
+				"    r.pebempl_empl_status = (SELECT MAX(NBRJOBS_STATUS) FROM NBRJOBS where NBRJOBS_pidm = r.pebempl_PIDM\r\n" + 
+				"    AND (nbrjobs_pict_code = 'ED' or nbrjobs_pict_code = 'LD' )   AND nbrjobs_effective_date = (SELECT MAX(nbrjobs_effective_date)\r\n" + 
+				"    FROM NBRJOBS where NBRJOBS_pidm = r.pebempl_PIDM AND   (nbrjobs_pict_code = 'ED'\r\n" + 
+				"    or nbrjobs_pict_code = 'LD' )))\r\n" + 
+				"AND PERAPPT_APPT_EFF_DATE =     (select MAX(PERAPPT_APPT_EFF_DATE)  from PERAPPT WHERE PERAPPT_PIDM = "+ id +")\r\n" + 
+				") )  = (('TITULAR AUXILIAR 1'))  THEN\r\n" + 
+				" into utic.uzmtverireq(uzmtverireq_id, uzmtreqplanm_id, PEAEMPL_PIDM) VALUES (3,1,"+ id +")\r\n" + 
+				" WHEN ((SELECT DISTINCT\r\n" + 
+				"(select max(ptrtenr_desc)  from PTRTENR where ptrtenr_code= PERAPPT_TENURE_CODE ) as CATEGORIA_ESCALAFON\r\n" + 
+				"FROM PEBEMPL r, PERAPPT\r\n" + 
+				"WHERE\r\n" + 
+				"r.pebempl_PIDM = PERAPPT.PERAPPT_PIDM\r\n" + 
+				"AND r.pebempl_empl_status = 'A'\r\n" + 
+				"--AND( r.pebempl_bcat_code = 'DO' )\r\n" + 
+				"AND r.pebempl_PIDM = "+id+"\r\n" + 
+				"AND (r.pebempl_bcat_code = 'DO' or r.pebempl_bcat_code = 'SP' ) AND\r\n" + 
+				"    r.pebempl_empl_status = (SELECT MAX(NBRJOBS_STATUS) FROM NBRJOBS where NBRJOBS_pidm = r.pebempl_PIDM\r\n" + 
+				"    AND (nbrjobs_pict_code = 'ED' or nbrjobs_pict_code = 'LD' )   AND nbrjobs_effective_date = (SELECT MAX(nbrjobs_effective_date)\r\n" + 
+				"    FROM NBRJOBS where NBRJOBS_pidm = r.pebempl_PIDM AND   (nbrjobs_pict_code = 'ED'\r\n" + 
+				"    or nbrjobs_pict_code = 'LD' )))\r\n" + 
+				"AND PERAPPT_APPT_EFF_DATE =     (select MAX(PERAPPT_APPT_EFF_DATE)  from PERAPPT WHERE PERAPPT_PIDM = "+ id +")\r\n" + 
+				") )  = (('TITULAR PRINCIPAL 1'))  THEN\r\n" + 
+				" into utic.uzmtverireq(uzmtverireq_id, uzmtreqplanm_id, PEAEMPL_PIDM) VALUES (3,1,"+ id +")\r\n" + 
+				" WHEN ((SELECT DISTINCT\r\n" + 
+				"(select max(ptrtenr_desc)  from PTRTENR where ptrtenr_code= PERAPPT_TENURE_CODE ) as CATEGORIA_ESCALAFON\r\n" + 
+				"FROM PEBEMPL r, PERAPPT\r\n" + 
+				"WHERE\r\n" + 
+				"r.pebempl_PIDM = PERAPPT.PERAPPT_PIDM\r\n" + 
+				"AND r.pebempl_empl_status = 'A'\r\n" + 
+				"--AND( r.pebempl_bcat_code = 'DO' )\r\n" + 
+				"AND r.pebempl_PIDM = "+ id +"\r\n" + 
+				"AND (r.pebempl_bcat_code = 'DO' or r.pebempl_bcat_code = 'SP' ) AND\r\n" + 
+				"    r.pebempl_empl_status = (SELECT MAX(NBRJOBS_STATUS) FROM NBRJOBS where NBRJOBS_pidm = r.pebempl_PIDM\r\n" + 
+				"    AND (nbrjobs_pict_code = 'ED' or nbrjobs_pict_code = 'LD' )   AND nbrjobs_effective_date = (SELECT MAX(nbrjobs_effective_date)\r\n" + 
+				"    FROM NBRJOBS where NBRJOBS_pidm = r.pebempl_PIDM AND   (nbrjobs_pict_code = 'ED'\r\n" + 
+				"    or nbrjobs_pict_code = 'LD' )))\r\n" + 
+				"AND PERAPPT_APPT_EFF_DATE =     (select MAX(PERAPPT_APPT_EFF_DATE)  from PERAPPT WHERE PERAPPT_PIDM = "+ id +")\r\n" + 
+				") )  = (('TITULAR AGREGADO 3'))  THEN\r\n" + 
+				" into utic.uzmtverireq(uzmtverireq_id, uzmtreqplanm_id, PEAEMPL_PIDM) VALUES (3,1,"+ id +")\r\n" + 
+				" WHEN ((SELECT DISTINCT\r\n" + 
+				"(select max(ptrtenr_desc)  from PTRTENR where ptrtenr_code= PERAPPT_TENURE_CODE ) as CATEGORIA_ESCALAFON\r\n" + 
+				"FROM PEBEMPL r, PERAPPT\r\n" + 
+				"WHERE\r\n" + 
+				"r.pebempl_PIDM = PERAPPT.PERAPPT_PIDM\r\n" + 
+				"AND r.pebempl_empl_status = 'A'\r\n" + 
+				"--AND( r.pebempl_bcat_code = 'DO' )\r\n" + 
+				"AND r.pebempl_PIDM = "+ id +"\r\n" + 
+				"AND (r.pebempl_bcat_code = 'DO' or r.pebempl_bcat_code = 'SP' ) AND\r\n" + 
+				"    r.pebempl_empl_status = (SELECT MAX(NBRJOBS_STATUS) FROM NBRJOBS where NBRJOBS_pidm = r.pebempl_PIDM\r\n" + 
+				"    AND (nbrjobs_pict_code = 'ED' or nbrjobs_pict_code = 'LD' )   AND nbrjobs_effective_date = (SELECT MAX(nbrjobs_effective_date)\r\n" + 
+				"    FROM NBRJOBS where NBRJOBS_pidm = r.pebempl_PIDM AND   (nbrjobs_pict_code = 'ED'\r\n" + 
+				"    or nbrjobs_pict_code = 'LD' )))\r\n" + 
+				"AND PERAPPT_APPT_EFF_DATE =     (select MAX(PERAPPT_APPT_EFF_DATE)  from PERAPPT WHERE PERAPPT_PIDM = "+ id +")\r\n" + 
+				") )  = (('TITULAR AGREGADO 1'))  THEN\r\n" + 
+				" into utic.uzmtverireq(uzmtverireq_id, uzmtreqplanm_id, PEAEMPL_PIDM) VALUES (3,1,"+ id +")\r\n" + 
+				" WHEN ((SELECT DISTINCT\r\n" + 
+				"(select max(ptrtenr_desc)  from PTRTENR where ptrtenr_code= PERAPPT_TENURE_CODE ) as CATEGORIA_ESCALAFON\r\n" + 
+				"FROM PEBEMPL r, PERAPPT\r\n" + 
+				"WHERE\r\n" + 
+				"r.pebempl_PIDM = PERAPPT.PERAPPT_PIDM\r\n" + 
+				"AND r.pebempl_empl_status = 'A'\r\n" + 
+				"--AND( r.pebempl_bcat_code = 'DO' )\r\n" + 
+				"AND r.pebempl_PIDM = "+ id +"\r\n" + 
+				"AND (r.pebempl_bcat_code = 'DO' or r.pebempl_bcat_code = 'SP' ) AND\r\n" + 
+				"    r.pebempl_empl_status = (SELECT MAX(NBRJOBS_STATUS) FROM NBRJOBS where NBRJOBS_pidm = r.pebempl_PIDM\r\n" + 
+				"    AND (nbrjobs_pict_code = 'ED' or nbrjobs_pict_code = 'LD' )   AND nbrjobs_effective_date = (SELECT MAX(nbrjobs_effective_date)\r\n" + 
+				"    FROM NBRJOBS where NBRJOBS_pidm = r.pebempl_PIDM AND   (nbrjobs_pict_code = 'ED'\r\n" + 
+				"    or nbrjobs_pict_code = 'LD' )))\r\n" + 
+				"AND PERAPPT_APPT_EFF_DATE =     (select MAX(PERAPPT_APPT_EFF_DATE)  from PERAPPT WHERE PERAPPT_PIDM = "+ id +")\r\n" + 
+				") )  = (('TITULAR AGREGADO 2') )  THEN\r\n" + 
+				" into utic.uzmtverireq(uzmtverireq_id, uzmtreqplanm_id, PEAEMPL_PIDM) VALUES (3,1,"+ id +")\r\n" + 
+				" select uzmtverireq_id, uzmtreqplanm_id, PEAEMPL_PIDM from utic.uzmtverireq";
 	System.out.println(q);
 		return (List<ReqplanmVo>) jdbcTemplate.query(q, new BeanPropertyRowMapper<>(ReqplanmVo.class));
 	}
